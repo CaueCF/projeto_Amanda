98c2cbb603525b0fc72f6d9f54ec44fe46574ad3
Cauê Castello Ferreira <cauecferreira@gmail.com>
2024-05-08 19:10:09 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tela/src/core/ui/widgets/tela_loader.dart';
-import 'package:tela/src/tela_app.dart';
+import 'package:tela/src/features/splash/splash_page.dart';
 
 void main() {
-  runApp(const ProviderScope(child: Tela()));
+  runApp(const ProviderScope(child: SplashPage()));
 }
